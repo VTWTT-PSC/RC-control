319473e1a1e5246b5aa398e6db18d9a5f7e7d149
Jack4891 <95482991+Jack4891@users.noreply.github.com>
2024-03-30 15:26:24 -0400

    :alien:

--- RC_control/RC_control.ino
+++ RC_control/RC_control.ino
@@ -6,7 +6,10 @@ void setup() {
   actuator.attach(9); // attach the actuator to Arduino pin 9 (PWM)
   actuator.writeMicroseconds(1000); // give the actuator a 1ms pulse to retract the arm (1000us = 1ms)
   delay(3000); // delay a few seconds to give the arm time to retract
+    Serial.begin(9600);
+
 }
+
 
 void setActuator(int percentage){
 
@@ -14,19 +17,15 @@ void setActuator(int percentage){
   x += percentage*10;
   actuator.writeMicroseconds(x); // 1ms pulse to retract the arm
 
-
 }
 
 void loop() {
-  // Extend and retract the actuator arm on a 5 second interval
-  // actuator.writeMicroseconds(2000); // 2ms pulse to extend the arm
-  // delay(3000);
-  // actuator.writeMicroseconds(1000); // 1ms pulse to retract the arm
-  // delay(3000);
 
 
-  setActuator(100);
-  delay(3000);
+
   setActuator(0);
-  delay(3000);
+  Serial.println("bruh");
+  delay(100);
+
+
 }
